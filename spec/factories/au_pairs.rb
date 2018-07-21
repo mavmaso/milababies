d3bce69e897827c7cd4328fe29e65ad5c1c13d9a
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-07-21 01:58:38 -0300

    ajustando configuracao do capybara

--- spec/factories/au_pairs.rb
+++ spec/factories/au_pairs.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :au_pair do
-    name "MyString"
-    email "MyString"
-    phone "MyString"
-    social_media "MyString"
-    CPF "MyString"
-    skills "MyText"
-    degree "MyString"
-    languages "MyString"
-    birth_date "2018-07-20"
-    city "MyString"
-    rate 1
+    name 'Nani'
+    email 'nani@baba.com'
+    phone '99988-1234'
+    social_media 'facebook.com/nanibaba'
+    CPF '54545199011'
+    skills 'Brincadeiras educativas'
+    degree 'Segundo grau completo'
+    languages 'Portugues, Ingles'
+    birth_date '1993-04-19'
+    city 'Londrina'
+    rate 10
   end
 end
