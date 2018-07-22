6b82886a9185cd68b877782425c00f1740f83af2
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-07-22 02:45:33 -0300

    filtrando por nome

--- app/controllers/au_pairs_controller.rb
+++ app/controllers/au_pairs_controller.rb
@@ -1,7 +1,21 @@
 class AuPairsController < ApplicationController
 
   def index
-    @aupairs = AuPair.all
+    search_query = get_search_query
+    if search_query
+      @aupairs = AuPair.where('name LIKE ?', "%#{search_query}%")
+    else
+      @aupairs = AuPair.all
+    end
   end
+
+  private
+
+    def get_search_query
+      if params[:query].present?
+        return params[:query]
+      end
+      false
+    end
 
 end
--- spec/features/user_list_au_pairs_spec.rb
+++ spec/features/user_list_au_pairs_spec.rb
@@ -26,5 +26,78 @@ feature 'User list aupairs' do
     expect(page).to have_content('Nenhuma babá encontrada')
   end
 
+  scenario 'and filter by name' do
+    nanny_3 = create(:au_pair)
+    nanny_4 = create(:au_pair)
 
+    visit root_path
+    click_on 'Encontre Babás'
+
+    fill_in 'Buscar por', with: nanny_4.name
+    click_on 'Buscar'
+
+    expect(page).to have_content('Babás')
+    expect(page).to have_content('Nanny #4')
+    expect(page).to have_content("Habilidades: #{nanny_4.skills}")
+    expect(page).to have_content("Formação: #{nanny_4.degree}")
+    expect(page).to have_content("Cidade: #{nanny_4.city}")
+
+    expect(page).not_to have_content('Nanny #3')
+  end
+
+  scenario 'and filter by firsts chars of name ' do
+    nanny_5 = create(:au_pair)
+    nanny_6 = create(:au_pair)
+
+    visit root_path
+    click_on 'Encontre Babás'
+
+    fill_in 'Buscar por', with: 'Nanny'
+    click_on 'Buscar'
+
+    expect(page).to have_content('Nanny #5')
+    expect(page).to have_content('Nanny #6')
+  end
+
+  scenario 'and filter by lasts chars of name' do
+    nanny_7 = create(:au_pair)
+    nanny_8 = create(:au_pair)
+
+    visit root_path
+    click_on 'Encontre Babás'
+
+    fill_in 'Buscar por', with: 'y #7'
+    click_on 'Buscar'
+
+    expect(page).to have_content('Nanny #7')
+    expect(page).not_to have_content('Nanny #8')
+  end
+
+  scenario 'and filter by name ignoring case' do
+    nanny_9 = create(:au_pair)
+    nanny_10 = create(:au_pair)
+
+    visit root_path
+    click_on 'Encontre Babás'
+
+    fill_in 'Buscar por', with: 'NANNY #10'
+    click_on 'Buscar'
+
+    expect(page).to have_content('Nanny #10')
+    expect(page).not_to have_content('Nanny #9')
+  end
+
+  scenario 'and filter by empty name' do
+    nanny_11 = create(:au_pair)
+    nanny_12 = create(:au_pair)
+
+    visit root_path
+    click_on 'Encontre Babás'
+
+    fill_in 'Buscar por', with: ''
+    click_on 'Buscar'
+
+    expect(page).to have_content('Nanny #11')
+    expect(page).to have_content('Nanny #12')
+  end
 end
