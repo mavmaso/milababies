e54d1b662d36b06899501bc7f1846c035e21498e
Henrique Morato <henrique.morato@campuscode.com.br>
2018-07-23 20:11:02 -0300

    rubocop

--- app/controllers/au_pairs_controller.rb
+++ app/controllers/au_pairs_controller.rb
@@ -1,18 +1,15 @@
 class AuPairsController < ApplicationController
-
   def index
-    search_query = get_search_query
-    if search_query
-      @aupairs = AuPair.where('name LIKE ?', "%#{search_query}%")
-    else
-      @aupairs = AuPair.all
-    end
+    @aupairs = if search_query
+                 AuPair.where('name LIKE ?', "%#{search_query}%")
+               else
+                 AuPair.all
+               end
   end
 
   private
 
-    def get_search_query
-      params[:query]
-    end
-
+  def search_query
+    params[:query]
+  end
 end
--- app/models/au_pair.rb
+++ app/models/au_pair.rb
@@ -1,5 +1,5 @@
 class AuPair < ApplicationRecord
-  has_attached_file :photo, styles: { thumb: "100x100#" }
+  has_attached_file :photo, styles: { thumb: '100x100#' }
 
-  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :photo, content_type: %r{\Aimage\/.*\z}
 end
