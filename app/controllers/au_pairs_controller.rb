f8a19f302f9df0941618b2f1ad07b94a2bd4372c
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-07-26 05:38:34 -0300

    Buscando por varios atributos

--- app/controllers/au_pairs_controller.rb
+++ app/controllers/au_pairs_controller.rb
@@ -1,7 +1,10 @@
 class AuPairsController < ApplicationController
   def index
     @aupairs = if search_query
-                 AuPair.where('name LIKE ?', "%#{search_query}%")
+                 AuPair.where(['name LIKE :query OR skills LIKE :query OR ' \
+                               'degree LIKE :query OR city LIKE :query OR ' \
+                               'languages LIKE :query',
+                               query: "%#{search_query}%"])
                else
                  AuPair.all
                end
