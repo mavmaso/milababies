6325f9721cf0a6c0892230e7e317869d0fca607f
antonio e luiz <lg.oliveira.lopes@gmail.com>
2018-07-26 22:34:33 -0300

    Toque Final

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :admins
   root to: 'home#index'
   get '/admin', to: 'home#admin', as: 'admin'
+  get '/quemsomos', to: 'home#who', as: 'who'
   resources :au_pairs, only: [:index, :new, :create, :show] do
     resources :testimonials, only: [:new, :create]
     resources :proposals, only: [:new, :create, :show]
